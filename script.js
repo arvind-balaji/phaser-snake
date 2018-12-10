4872e46d197e11159eefbc0bedc82cb409b206ea
Arvind Balaji <arvindb02@gmail.com>
2018-12-10 09:32:06 -0600

    snek things

--- script.js
+++ script.js
@@ -1,87 +1,229 @@
 var config = {
-  type: Phaser.AUTO,
-  width: 1024,
-  height: 768,
-  backgroundColor: '#cccc00',
-  physics: {
-      default: 'arcade',
-      arcade: {
-          debug: false
-      }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+    type: Phaser.WEBGL,
+    width: 640,
+    height: 480,
+    backgroundColor: '#f44286',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
+var snake;
+var food;
+var cursors;
+
+//  Direction consts
+var UP = 0;
+var DOWN = 1;
+var LEFT = 2;
+var RIGHT = 3;
+
 var game = new Phaser.Game(config);
-var coolSnek;
-function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude',
-    'char.png', {
-      frameWidth: 32,
-      frameHeight: 32
-    })
-  }
 
-  function create() {
-    //this.add.image(400, 300, 'sky');
-      cursors = this.input.keyboard.createCursorKeys();
-      coolSnek = this.physics.add.sprite(100, 450, 'dude');
-          coolSnek.setCollideWorldBounds(true);
+function preload ()
+{
+    this.load.image('food', 'assets/food.png');
+    this.load.image('body', 'assets/body.png');
+}
 
-  }
-  var leftMove;
-  var upMove;
-  var rightMove;
-  var downMove;
-  function update()
-  {
+function create ()
+{
+    var Food = new Phaser.Class({
+
+      Extends: Phaser.GameObjects.Image,
+
+      initialize: function Food (scene, x, y)
+      {
+          Phaser.GameObjects.Image.call(this, scene)
+
+          this.setTexture('food');
+          this.setPosition(x * 16, y * 16);
+          this.setOrigin(0);
+
+          this.total = 0;
+
+          scene.children.add(this);
+      },
+      eat: function ()
+        {
+            this.total++;
+
+            var x = Phaser.Math.Between(0, 40);
+            var y = Phaser.Math.Between(0, 30);
+
+            this.setPosition(x * 16, y * 16);
+        }
+    });
+    var Snake = new Phaser.Class({
+
+        initialize: function Snake (scene, x, y)
+        {
+            this.headPosition = new Phaser.Geom.Point(x, y);
+            this.tail = new Phaser.Geom.Point(x, y);
+
+            this.body = scene.add.group();
 
 
-    if (cursors.left.isDown && !rightMove)
+            this.head = this.body.create(x * 16, y * 16, 'body');
+            this.head.setOrigin(0);
+
+            this.alive = true;
+
+            this.speed = 100;
+
+            this.moveTime = 0;
+
+            this.heading = RIGHT;
+            this.direction = RIGHT;
+        },
+
+        update: function (time)
+        {
+            if (time >= this.moveTime)
+            {
+                return this.move(time);
+            }
+        },
+
+        faceLeft: function ()
+        {
+            if (this.direction === UP || this.direction === DOWN)
+            {
+                this.heading = LEFT;
+            }
+        },
+
+        faceRight: function ()
+        {
+            if (this.direction === UP || this.direction === DOWN)
+            {
+                this.heading = RIGHT;
+            }
+        },
+
+        faceUp: function ()
+        {
+            if (this.direction === LEFT || this.direction === RIGHT)
+            {
+                this.heading = UP;
+            }
+        },
+
+        faceDown: function ()
+        {
+            if (this.direction === LEFT || this.direction === RIGHT)
+            {
+                this.heading = DOWN;
+            }
+        },
+
+        move: function (time)
+        {
+            /**
+            * Based on the heading property (which is the direction the pgroup pressed)
+            * we update the headPosition value accordingly.
+            *
+            * The Math.wrap call allow the snake to wrap around the screen, so when
+            * it goes off any of the sides it re-appears on the other.
+            */
+            switch (this.heading)
+            {
+                case LEFT:
+                    this.headPosition.x--;
+                    break;
+
+                case RIGHT:
+                    this.headPosition.x++;
+                    break;
+
+                case UP:
+                    this.headPosition.y--;
+                    break;
+
+                case DOWN:
+                    this.headPosition.y++;
+                    break;
+            }
+
+            this.direction = this.heading;
+
+            //  Update the body segments
+            Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1);
+
+            //  Update the timer ready for the next movement
+            this.moveTime = time + this.speed;
+
+            return true;
+        },
+        grow: function()
+        {
+          var newPart = this.body.create(this.tail.x, this.tail.y, 'body');
+
+             newPart.setOrigin(0);
+        },
+
+        collideWithFood: function (food)
+        {
+            if (this.head.x === food.x && this.head.y === food.y)
+            {
+                this.grow();
+
+                food.eat();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    });
+
+    snake = new Snake(this, 8, 8);
+    food = new Food(this, 10, 10)
+    //  Create our keyboard controls
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update (time, delta)
+{
+    if (!snake.alive)
     {
-      leftMove = true;
-      rightMove = false;
-      upMove = false;
-      downMove = false;
-      coolSnek.setVelocityY(0);
-      coolSnek.setVelocityX(-160);
-
-    }
-    else if (cursors.right.isDown && !leftMove)
-    {
-      upMove = false;
-      downMove = false;
-      rightMove = true;
-      leftMove = false;
-
-      coolSnek.setVelocityY(0);
-        coolSnek.setVelocityX(160);
-    }
-    else if(cursors.down.isDown && !upMove)
-    {
-      leftMove = false;
-      rightMove = false;
-      upMove = false;
-      downMove = true;
-        coolSnek.setVelocityX(0);
-        coolSnek.setVelocityY(+160);
-    }
-    else if(cursors.up.isDown && !downMove)
-    {
-      leftMove = false;
-      rightMove = false;
-      upMove = true;
-      downMove = false;
-          coolSnek.setVelocityX(0);
-          coolSnek.setVelocityY(-160);
+        return;
     }
 
+    /**
+    * Check which key is pressed, and then change the direction the snake
+    * is heading based on that. The checks ensure you don't double-back
+    * on yourself, for example if you're moving to the right and you press
+    * the LEFT cursor, it ignores it, because the only valid directions you
+    * can move in at that time is up and down.
+    */
+    if (cursors.left.isDown)
+    {
+        snake.faceLeft();
+    }
+    else if (cursors.right.isDown)
+    {
+        snake.faceRight();
+    }
+    else if (cursors.up.isDown)
+    {
+        snake.faceUp();
+    }
+    else if (cursors.down.isDown)
+    {
+        snake.faceDown();
+    }
+
+    if (snake.update(time))
+     {
+         //  If the snake updated, we need to check for collision against food
+         snake.collideWithFood(food);
+     }
 }
